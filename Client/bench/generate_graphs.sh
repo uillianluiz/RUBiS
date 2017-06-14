499d4382c619b733cc01cbb3b9ecf0c67e367dc6
Uillian <uillianludwig@gmail.com>
2017-06-14 17:00:52 -0300

    fix mkdir script

--- Client/bench/generate_graphs.sh
+++ Client/bench/generate_graphs.sh
@@ -2,31 +2,31 @@
 
 # $1 is the report directory with the trailing /
 
-mkdir $1data
+mkdir $1data;
 
-awk '/CPU/{getline; print}' $1client0 | sed '1d; $d'  > $1data/client0_cpu.csv;
-awk '/proc\/s/{getline; print}' $1client0 | sed '1d; $d'  > $1data/client0_proc.csv;
-awk '/pgpgin\/s/{getline; print}' $1client0 | sed '1d; $d'  > $1data/client0_pagination.csv;
-awk '/tps/{getline; print}' $1client0 | sed '1d; $d'  > $1data/client0_io.csv;
-awk '/kbmemfree/{getline; print}' $1client0 | sed '1d; $d'  > $1data/client0_memory.csv;
-awk '/IFACE/{getline; print}' $1client0 | sed '1d; $d'  > $1data/client0_network.csv;
-awk '/totsck/{getline; print}' $1client0 | sed '1d; $d'  > $1data/client0_sockets.csv;
-awk '/MBfsfree/{getline; print}' $1client0 | sed '1d; $d'  > $1data/client0_storage.csv;
+awk '/CPU/{getline; print}' $1client0 | sed '1d; $d'  > $1data/client0_cpu;
+awk '/proc\/s/{getline; print}' $1client0 | sed '1d; $d'  > $1data/client0_proc;
+awk '/pgpgin\/s/{getline; print}' $1client0 | sed '1d; $d'  > $1data/client0_pagination;
+awk '/tps/{getline; print}' $1client0 | sed '1d; $d'  > $1data/client0_io;
+awk '/kbmemfree/{getline; print}' $1client0 | sed '1d; $d'  > $1data/client0_memory;
+awk '/IFACE/{getline; print}' $1client0 | sed '1d; $d'  > $1data/client0_network;
+awk '/totsck/{getline; print}' $1client0 | sed '1d; $d'  > $1data/client0_sockets;
+awk '/MBfsfree/{getline; print}' $1client0 | sed '1d; $d'  > $1data/client0_storage;
 
-awk '/CPU/{getline; print}' $1db_server | sed '1d; $d'  > $1data/db_server_cpu.csv;
-awk '/proc\/s/{getline; print}' $1db_server | sed '1d; $d'  > $1data/db_server_proc.csv;
-awk '/pgpgin\/s/{getline; print}' $1db_server | sed '1d; $d'  > $1data/db_server_pagination.csv;
-awk '/tps/{getline; print}' $1db_server | sed '1d; $d'  > $1data/db_server_io.csv;
-awk '/kbmemfree/{getline; print}' $1db_server | sed '1d; $d'  > $1data/db_server_memory.csv;
-awk '/IFACE/{getline; print}' $1db_server | sed '1d; $d'  > $1data/db_server_network.csv;
-awk '/totsck/{getline; print}' $1db_server | sed '1d; $d'  > $1data/db_server_sockets.csv;
-awk '/MBfsfree/{getline; print}' $1db_server | sed '1d; $d'  > $1data/db_server_storage.csv;
+awk '/CPU/{getline; print}' $1db_server | sed '1d; $d'  > $1data/db_server_cpu;
+awk '/proc\/s/{getline; print}' $1db_server | sed '1d; $d'  > $1data/db_server_proc;
+awk '/pgpgin\/s/{getline; print}' $1db_server | sed '1d; $d'  > $1data/db_server_pagination;
+awk '/tps/{getline; print}' $1db_server | sed '1d; $d'  > $1data/db_server_io;
+awk '/kbmemfree/{getline; print}' $1db_server | sed '1d; $d'  > $1data/db_server_memory;
+awk '/IFACE/{getline; print}' $1db_server | sed '1d; $d'  > $1data/db_server_network;
+awk '/totsck/{getline; print}' $1db_server | sed '1d; $d'  > $1data/db_server_sockets;
+awk '/MBfsfree/{getline; print}' $1db_server | sed '1d; $d'  > $1data/db_server_storage;
 
-awk '/CPU/{getline; print}' $1web_server | sed '1d; $d'  > $1data/web_server_cpu.csv;
-awk '/proc\/s/{getline; print}' $1web_server | sed '1d; $d'  > $1data/web_server_proc.csv;
-awk '/pgpgin\/s/{getline; print}' $1web_server | sed '1d; $d'  > $1data/web_server_pagination.csv;
-awk '/tps/{getline; print}' $1web_server | sed '1d; $d'  > $1data/web_server_io.csv;
-awk '/kbmemfree/{getline; print}' $1web_server | sed '1d; $d'  > $1data/web_server_memory.csv;
-awk '/IFACE/{getline; print}' $1web_server | sed '1d; $d'  > $1data/web_server_network.csv;
-awk '/totsck/{getline; print}' $1web_server | sed '1d; $d'  > $1data/web_server_sockets.csv;
-awk '/MBfsfree/{getline; print}' $1web_server | sed '1d; $d'  > $1data/web_server_storage.csv;
+awk '/CPU/{getline; print}' $1web_server | sed '1d; $d'  > $1data/web_server_cpu;
+awk '/proc\/s/{getline; print}' $1web_server | sed '1d; $d'  > $1data/web_server_proc;
+awk '/pgpgin\/s/{getline; print}' $1web_server | sed '1d; $d'  > $1data/web_server_pagination;
+awk '/tps/{getline; print}' $1web_server | sed '1d; $d'  > $1data/web_server_io;
+awk '/kbmemfree/{getline; print}' $1web_server | sed '1d; $d'  > $1data/web_server_memory;
+awk '/IFACE/{getline; print}' $1web_server | sed '1d; $d'  > $1data/web_server_network;
+awk '/totsck/{getline; print}' $1web_server | sed '1d; $d'  > $1data/web_server_sockets;
+awk '/MBfsfree/{getline; print}' $1web_server | sed '1d; $d'  > $1data/web_server_storage;
