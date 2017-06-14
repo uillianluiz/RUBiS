b8c3a4ef7ac577c2c80b1d9bb5d61051887c8b1d
Uillian <uillianluiz@yahoo.com.br>
2017-06-13 22:41:51 -0300

    remove duplicate files

--- bench/node_info.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/tcsh
-
-/bin/echo "Host  : "`/bin/hostname`
-/bin/echo "Kernel: "`/bin/cat /proc/version`
-/usr/bin/lspci | grep net
-/usr/bin/awk '$0 ~ /processor|vendor_id|model name|cpu MHz|cache/' /proc/cpuinfo
-/bin/grep MemTotal /proc/meminfo
-/bin/grep SwapTotal /proc/meminfo
