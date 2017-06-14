3cc0af0dcda390ca1f88bbadc68c1104aed88262
Uillian <uillianluiz@yahoo.com.br>
2017-06-13 22:04:56 -0300

    fix node info script

--- bench/node_info.sh
+++ bench/node_info.sh
@@ -2,7 +2,7 @@
 
 /bin/echo "Host  : "`/bin/hostname`
 /bin/echo "Kernel: "`/bin/cat /proc/version`
-/bin/grep net /proc/pci
+/usr/bin/lspci | grep net
 /usr/bin/awk '$0 ~ /processor|vendor_id|model name|cpu MHz|cache/' /proc/cpuinfo
 /bin/grep MemTotal /proc/meminfo
 /bin/grep SwapTotal /proc/meminfo
